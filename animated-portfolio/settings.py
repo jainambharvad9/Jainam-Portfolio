f736b732c20a9a640d34e5cc5abe91e7a49a0356
jainambharvad9 <jainambharvad9@gmail.com>
2025-10-05 16:43:45 +0530

    ready for render

--- animated-portfolio/settings.py
+++ animated-portfolio/settings.py
@@ -14,7 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-development')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+# DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
